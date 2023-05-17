7c0674c7049ff1157b809755ea2c21eb96fb57c1
Darya <daryakolisnik@gmail.com>
2023-05-17 16:42:56 +0300

    update Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,15 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      <Profile array={["username": "Jacques Gluke",
-  "tag": "jgluke",
-  "location": "Ocho Rios, Jamaica",
-  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  "stats": {
-    "followers": 5603,
-    "views": 4827,
-    "likes": 1308
-  }]} />
+      <Profile />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,31 @@
 import css from './Profile.module.css'
+import userData from './user.json'
 
-export const Profile = ({array}) => {
-    return (<div className={css.profile} key='profile-id'>
+export const Profile = () => {
+  const { username, tag, location, avatar, stats } = userData;
+  return (<div className={css.profile} key='profile-id'>
         <div className={css.description}>
-    <img
-                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-                alt="User avatar"
-                className={css.avatar}
-    />
-            <p className={css.name}>Petra Marica</p>
-    <p className={css.tag}>@pmarica</p>
-            <p className={css.location}>Salvador, Brasil</p>
+     <img
+          src={avatar}
+          alt="User avatar"
+          className={css.avatar} />
+            <p className={css.name}>{username}</p>
+    <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>
   </div>
 
         <ul className={css.stats}>
     <li className={css.liStats}>
                 <span className={css.label}>Followers</span>
-                <span className={css.quantity}>1000</span>
+                <span className={css.quantity}>{stats.followers}</span>
     </li>
     <li className={css.liStats}>
                 <span className={css.label}>Views</span>
-                <span className={css.quantity}>2000</span>
+                <span className={css.quantity}>{stats.views}</span>
     </li>
     <li className={css.liStats}>
                 <span className={css.label}>Likes</span>
-                <span className={css.quantity}>3000</span>
+                <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
